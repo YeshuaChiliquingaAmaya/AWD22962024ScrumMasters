2318db526d6ac0b62ee92efe056ee0f1d01a7a6e
Jeremy Alejandro Cuadrado Logacho <alejocuadrado1@gmail.com>
2024-11-21 19:26:05 -0500

    Code_V1

---
+++ 06-Code/Project/padres.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Teacher</title>
+
+<link href="css/estilo.css" rel="stylesheet" type="text/css">
+<link href="css/estilo_barra.css" rel="stylesheet" type="text/css">
+<link href="css/estilo_footer.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<?php include 'barra_navegacion.php'; ?>
+
+    <header style="text-align: left; padding: 2px;">
+        <img src="img/imagen.jpg" alt="Header Image" style="max-width: 4%; height: auto;">
+    </header>
+
+    <!-- Línea horizontal decorativa -->
+    <hr size="10" width="100%" noshade color="blue">
+    <hr size="10" width="100%" noshade color="#2F0CEC">
+    <h1>PAGINA PADRES</h1>
+    <!-- Párrafo introductorio -->
+<p><strong>Teacher Name:</strong> Juan Hurtado.</p>
+
+    <hr size="10" width="100%" noshade color="#2F0CEC">
+
+    <h1>Information</h1>
+    <p>
+        A page will be created that manages the grades of the institution, this includes the grades of the students, their weightings of the grades, as well as includes what grades the following divisions will be taken into account for the system: <strong>“Tests, workshops, assignments, homework, exams”</strong>
+    </p>
+    <pre>
+Educational centers continue to constantly change, 
+which is why a platform that allows them to facilitate
+the daily work within the reach of teachers and students,
+our team is happy to work with the educational unit and 
+its workers to satisfy their needs.
+    </pre>
+    <hr size="10" width="100%" noshade color="blue">
+
+    <!-- Nota final -->
+    <p>Support Team</p>
+	<?php include 'footer.php'; ?>
+</body>
+</html>
